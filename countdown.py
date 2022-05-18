71ab05ee8ae1dbd39855df8714c22710c0248f14
Milind Sidhu <milindsidhu@gmail.com>
2022-05-19 00:58:24 +0530

    modified countdown.py

--- countdown.py
+++ countdown.py
@@ -9,6 +9,8 @@ deadline_date = input_list[1]
 deadline_date = datetime.datetime.strptime(deadline_date, "%d.%m.%Y")
 today_date = datetime.datetime.today()
 time_till = deadline_date - today_date
+# if you don't want to have the time with hours&minutes, then use time_till.days
+# ending with "..........{time_till.days} days" as it won't show "days" info.
+print(f"The time remaining for your goal: {goal} is {time_till}")
 
-print("The time remaining for your goal is", time_till)
 
